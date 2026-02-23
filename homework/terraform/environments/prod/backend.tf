866aa3382967394ab9663a56d4b079345f988152
Vasil Viarbitski <v.viarbitski@godeltech.com>
2026-02-23 12:29:41 +0100

    Fix empty S3 backend blocks - add placeholder key
    Terraform doesn't allow completely empty backend blocks in some versions.
    Added placeholder 'key' parameter with comment explaining it gets
    overridden by backend.hcl during terraform init.
    
    This allows partial backend configuration via -backend-config flag
    while satisfying Terraform's syntax requirements.

--- homework/terraform/environments/prod/backend.tf
+++ homework/terraform/environments/prod/backend.tf
@@ -1,3 +1,7 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    # Configuration provided via backend.hcl at init time
+    # See: terraform init -backend-config=backend.hcl
+    key = "placeholder" # Overridden by backend-config
+  }
 }
